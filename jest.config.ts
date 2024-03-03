2d9ee0a95848d1910c921fc83ea99ba633daa609
Sai Teja <saiteja.g1801@gmail.com>
2024-03-03 21:07:24 +0530

    Jest config setup added with coverage and demo test for homepage

---
+++ jest.config.ts
@@ -0,0 +1,26 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from "jest";
+
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+const config: Config = {
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  // Add more setup options before each test is run
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(config);
