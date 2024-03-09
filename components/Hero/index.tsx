6808f9212ff53a324bfa130ccdc0b763bc64e8a8
Abhishek7Tech <abhishektab45@gmail.com>
2024-03-09 19:04:25 +0530

    updated fonts

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -13,10 +13,10 @@ function Hero() {
                     className='h-80 w-full rounded-img md:w-3/5'
                 ></Image>
                 <div className='flex flex-col items-center p-4 md:items-start md:p-8 lg:w-5/12'>
-                    <h1 className='font-logo text-3xl  font-bold leading-10 md:text-5xl md:leading-tight'>
+                    <h1 className='font-main text-3xl font-bold leading-10 md:text-5xl md:leading-tight'>
                         Find your perfect dog breed.
                     </h1>
-                    <p className='font-sans text-lg font-medium leading-8 md:leading-10'>
+                    <p className='font-sans text-lg leading-8 md:leading-10'>
                         Discover the ideal companion for your lifestyle.
                     </p>
                     <SearchBar />
