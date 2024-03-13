1e00db233d1c3dab7030dadc8dcd2545516a6112
Abhishek7Tech <abhishektab45@gmail.com>
2024-03-13 17:37:18 +0530

    hero component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -15,7 +15,7 @@ import { FaArrowRightLong } from 'react-icons/fa6'
 function Header() {
     const isLoggedIn = false
     return (
-        <header className='text-secondary-body sticky inset-0 inset-y-0 right-0 w-full border-b bg-background px-4 py-3 dark:text-white md:px-12'>
+        <header className='sticky inset-0 inset-y-0 right-0 w-full border-b bg-background px-4 py-3 text-secondary-body dark:text-white md:px-12'>
             <nav className='flex items-center justify-between'>
                 <Link href='/'>
                     <div className='flex items-center justify-between space-x-3'>
@@ -31,7 +31,7 @@ function Header() {
                 </Link>
                 <div className=' flex flex-row gap-8'>
                     <div className='hidden md:flex'>
-                        <ul className='flex gap-[1.2rem] font-main'>
+                        <ul className='font-main flex gap-[1.2rem]'>
                             {navLinks.map((navLink, index) => (
                                 <li key={index}>
                                     <NavLink href={navLink.src}>
@@ -90,7 +90,7 @@ function Header() {
 
                             <div className='mt-8 flex flex-col gap-6'>
                                 <div className='flex'>
-                                    <ul className='-mx-2 flex flex-col gap-[1.2rem] font-main'>
+                                    <ul className='font-main -mx-2 flex flex-col gap-[1.2rem]'>
                                         {navLinks.map((navLink, index) => (
                                             <li key={index}>
                                                 <NavLink href={navLink.src}>
--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -2,17 +2,25 @@
 import Image from 'next/image'
 import HeroImg from '@/assets/image/hero.jpg'
 import SearchBar from '../ui/search-bar'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card'
 
 function Hero() {
     return (
         <section className='mx-4 my-8 w-10/12 text-black dark:text-white md:m-auto md:my-16'>
-            <div className='flex flex-col justify-around pl-20 md:flex-row'>
+            {/* <div className='flex flex-col justify-around pl-20 md:flex-row'>
                 <Image
                     src={HeroImg}
                     alt='an image of dog running with joy.'
                     className='h-80 w-full rounded-img md:w-3/5'
                 ></Image>
-                <div className='flex flex-col items-center p-4 md:items-start md:p-8 lg:w-5/12'>
+                <div className='lg:w-5/12 flex flex-col items-center p-4 md:items-start md:p-8'>
                     <h1 className='font-main text-3xl font-bold leading-10 md:text-5xl md:leading-tight'>
                         Find your perfect dog breed.
                     </h1>
@@ -21,7 +29,26 @@ function Hero() {
                     </p>
                     <SearchBar />
                 </div>
-            </div>
+            </div> */}
+
+            <Card className='mx-auto flex flex-col justify-around border-none shadow-none md:flex-row'>
+                {/* <CardHeader > */}
+                <Image
+                    src={HeroImg}
+                    alt='an image of dog running with joy.'
+                    className='h-80 w-full rounded-img md:w-3/5'
+                ></Image>
+                {/* </CardHeader> */}
+                <CardContent className='lg:w-5/12 flex flex-col items-center p-4 md:items-start md:p-8'>
+                    <h1 className='font-main text-3xl font-bold leading-10 md:text-5xl md:leading-tight'>
+                        Find your perfect dog breed.
+                    </h1>
+                    <p className='font-sans text-lg leading-8 md:leading-10'>
+                        Discover the ideal companion for your lifestyle.
+                    </p>
+                    <SearchBar />
+                </CardContent>
+            </Card>
         </section>
     )
 }
