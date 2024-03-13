bcc0ecc4a266b14cbad50310b79d8654094fa082
Abhishek7Tech <abhishektab45@gmail.com>
2024-03-13 18:07:44 +0530

    hero component updates

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,3 +1,38 @@
-function Hero() {}
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card'
+
+;('use client')
+import Image from 'next/image'
+import HeroImg from '@/assets/image/hero.jpg'
+import SearchBar from '../ui/search-bar'
+
+function Hero() {
+    ;<section className='mx-4 my-8 w-10/12 text-black dark:text-white md:m-auto md:my-16'>
+        <Card className='mx-auto flex flex-col justify-around border-none shadow-none md:flex-row'>
+            {/* <CardHeader > */}
+            <Image
+                src={HeroImg}
+                alt='an image of dog running with joy.'
+                className='h-80 w-full rounded-img md:w-3/5'
+            ></Image>
+            {/* </CardHeader> */}
+            <CardContent className='lg:w-5/12 flex flex-col items-center p-4 md:items-start md:p-8'>
+                <h1 className='font-main text-3xl font-bold leading-10 md:text-5xl md:leading-tight'>
+                    Find your perfect dog breed.
+                </h1>
+                <p className='font-sans text-lg leading-8 md:leading-10'>
+                    Discover the ideal companion for your lifestyle.
+                </p>
+                <SearchBar />
+            </CardContent>
+        </Card>
+    </section>
+}
 
 export default Hero
