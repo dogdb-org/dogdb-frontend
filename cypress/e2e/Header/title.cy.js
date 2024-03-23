bce3f06a2c4dcececd1ab6a4a460757620426a3a
adamrichardturner <hello@adamrichardturner.dev>
2024-03-23 07:26:43 +0000

    Config for static builds

--- cypress/e2e/Header/title.cy.js
+++ cypress/e2e/Header/title.cy.js
@@ -12,16 +12,16 @@
 // https://on.cypress.io/introduction-to-cypress
 
 describe('Header Component', () => {
-  beforeEach(() => {
-    // Cypress starts out with a blank slate for each test
-    // so we must tell it to visit our website with the `cy.visit()` command.
-    // Since we want to visit the same URL at the start of all our tests,
-    // we include it in our beforeEach function so that it runs before each test
-    cy.visit('http://localhost:3000')
-  })
+    beforeEach(() => {
+        // Cypress starts out with a blank slate for each test
+        // so we must tell it to visit our website with the `cy.visit()` command.
+        // Since we want to visit the same URL at the start of all our tests,
+        // we include it in our beforeEach function so that it runs before each test
+        cy.visit('http://localhost:3000')
+    })
 
-  it('cy.title() - get the title', () => {
-      // https://on.cypress.io/title
-      cy.title().should('include', 'DogDB')
-  })
+    it('cy.title() - get the title', () => {
+        // https://on.cypress.io/title
+        cy.title().should('include', 'DogDB')
+    })
 })
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     // Image optimization configuration
     images: {
         remotePatterns: [
