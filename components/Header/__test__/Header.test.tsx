9e7d4cd19565de5f4041a7452637489dad617ea7
musa108 <nmanagi23@gmail.com>
2024-04-02 12:31:43 +0100

    update: wrote a jest test for the header component

---
+++ components/Header/__test__/Header.test.tsx
@@ -0,0 +1,24 @@
+import '@testing-library/jest-dom'
+import { render } from '@testing-library/react'
+import Header from '../Header'
+
+const { expect, describe, it } = require('@jest/globals')
+
+describe('Header Component', () => {
+    it('renders logo and title correctly', () => {
+        const { getByAltText, getByText } = render(<Header />)
+        expect(
+            getByAltText('an image of a dog in an orange color')
+        ).toBeInTheDocument()
+        expect(getByText('dogdb')).toBeInTheDocument()
+    })
+
+    it('renders navigation links correctly', () => {
+        const { getByText } = render(<Header />)
+        expect(getByText('Home')).toBeInTheDocument()
+        expect(getByText('Breeds')).toBeInTheDocument()
+        expect(getByText('About')).toBeInTheDocument()
+        expect(getByText('My Dogs')).toBeInTheDocument()
+        expect(getByText('Donate')).toBeInTheDocument()
+    })
+})
